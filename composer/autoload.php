401f7798e5ab7d10847e2d7c7231f66bbe070616
PC <val_b@orange.fr>
2025-12-24 14:36:27 +0100

    test

--- composer/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// SPDX-FileCopyrightText: Your Name <your@email.com>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-//
-declare(strict_types=1);
-
-require_once __DIR__ . '/../vendor/autoload.php';
